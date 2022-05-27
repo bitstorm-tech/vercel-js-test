af86bb81143166631171be927003fcc06c2fdb58
Josef Bauer <josef.bauer.1st@gmail.com>
2022-05-27 13:59:06 +0200

    Only use random code

--- src/routes/api/accounts.js
+++ src/routes/api/accounts.js
@@ -1,12 +1,12 @@
 import { randomString } from "$lib/foobar";
-import prismaClient from "../../lib/prisma";
 
 export async function get() {
-  const accounts = await prismaClient.account.findMany();
+  //   const accounts = await prismaClient.account.findMany();
   const rndStr = randomString();
   console.log("Random String:", rndStr);
-  console.log("Acounts:", accounts);
+  //   console.log("Acounts:", accounts);
   return {
-    body: accounts,
+    // body: accounts,
+    body: rndStr,
   };
 }
