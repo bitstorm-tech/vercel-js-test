935777c91f2a5a28d9bae489721b9944df087893
Josef Bauer <josef.bauer.1st@gmail.com>
2022-05-29 20:08:21 +0200

    Log prismaClient

--- src/routes/api/accounts.js
+++ src/routes/api/accounts.js
@@ -1,6 +1,7 @@
 import prismaClient from "$lib/prisma";
 
 export async function get() {
+  console.log("prismaClient:", prismaClient);
   const accounts = await prismaClient.account.findMany();
   return {
     body: accounts,
